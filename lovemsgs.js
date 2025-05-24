2396d4aa57715a913ad4fafeb50da540306738a7
Emmanuel B <blancocemmanuel@gmail.com>
2025-05-24 23:11:18 +1000

    final commit Love Messages

--- lovemsgs.js
+++ lovemsgs.js
@@ -5,73 +5,77 @@ function randomWord (num) {
 
 const loveMessages = {
     line1Verb: ['farts', 'defecates', 'slaps', 'burns'],
-    line2Verb: ['runs', 'vomits', 'yells', 'spanks'],
+    line2Verb: ['stings', 'vomits', 'yells', 'spanks'],
     line3Verb: ['dies', 'sings', 'showers', 'crawls'],
     line4Verb: ['licks', 'tickles', 'kills', 'bites'],
 }
 
 const loveAdjectives = {
     line1Adj: ['fluffy', 'squishy', 'wobbly', 'goopy'],
-    line2Adj: [ 'crusty', 'soggy', 'toasty', 'gravy-scented'],
-    line3Adj: ['pickle-colored', 'glitter-infested', 'hard','smelly'],
+    line2Adj: [ 'crusty', 'soggy', 'toasty', 'sewage-scented'],
+    line3Adj: ['poop-colored', 'glitter-infested', 'sour','smelly'],
     line4Adj: ['itchy', 'annoying', 'abandoned', 'spat']
 }
 
-let line1 = [];
-let line2 = [];
-let line3 = [];
-let line4 = [];
+let theLine = [];
+
 
 for (let wording in loveMessages) {
     let indexedWord = randomWord(loveMessages[wording].length) 
    
    switch (wording) {
-   case 'line1Verb':  // the below code extracts the value in each key in a random index
-   line1.push(`she`, `${loveMessages[wording][indexedWord]}`, `your`, `name`, `in`, `the`, `adjective`, `air,`)
+   case 'line1Verb':  // the below code extracts the value in each key in the random index
+   theLine.push(`she`, `${loveMessages[wording][indexedWord]}`, `your`, `name`, `in`, `the`, `adjective`, `air,`)
    break;
    case 'line2Verb':
-   line2.push(`she`, `${loveMessages[wording][indexedWord]}`, `like`, `adjective`, `stars,`)
+   theLine.push(`she`, `${loveMessages[wording][indexedWord]}`, `like`, `adjective`, `stars,`)
    break;
    case 'line3Verb':
-   line3.push(`and`, `my`, `heart`, `${loveMessages[wording][indexedWord]}`, `in`, `adjective`, `pleas,`) 
+   theLine.push(`and`, `my`, `heart`, `${loveMessages[wording][indexedWord]}`, `in`, `adjective`, `pleas,`) 
    break;
    case 'line4Verb':
-   line4.push(`Your`, `touch`, `${loveMessages[wording][indexedWord]}`, `my`, `adjective`, `soul.`)
+   theLine.push(`Your`, `touch`, `${loveMessages[wording][indexedWord]}`, `my`, `adjective`, `soul.`)
    break;
    default:
    console.log("Not enough Information")   
 } 
            
 }
+//console.log(theLine);
 
-let index1 =[]
-let index2 =[]
-let index3 =[]
-let index4 =[]
-for (let i = 0; i < 4; i++) {
-    index1 = line1.indexOf('adjective');
-    index2 = line2.indexOf('adjective');
-    index3 = line3.indexOf('adjective');
-    index4 = line4.indexOf('adjective');
+let theIndex =[]
+let adjective = ['adjective']
+for (let i = 0; i < theLine.length; i++) {
+    if (adjective.includes(theLine[i])) {
+        theIndex.push(i);
+    }
+   
 }
 
-
+theIndex = theIndex.flat();
+//console.log(theIndex);
+//notice 'adjective is compares to the array word as a string not as an array beacuse when comparing a string in an array to an array it would not work. 
+/*let adjective = 'adjective' //alternative for loop for the above 
+for (let i = 0; i < theLine.length; i++) { 
+    if (theLine[i] === adjective) {
+        theIndex.push(i);
+    }*/
 
 
 for (words in loveAdjectives) {
     let ramWord = randomWord(loveAdjectives[words].length)
     switch (words) {
     case 'line1Adj':
-    line1.splice(index1, 1, loveAdjectives[words][ramWord])
+    theLine.splice(theIndex[0], 1, loveAdjectives[words][ramWord])
     break;
     case 'line2Adj':
-    line2.splice(index2, 1, loveAdjectives[words][ramWord])
+    theLine.splice(theIndex[1], 1, loveAdjectives[words][ramWord])
     break;
     case 'line3Adj':
-    line3.splice(index3, 1, loveAdjectives[words][ramWord])  
+    theLine.splice(theIndex[2], 1, loveAdjectives[words][ramWord])  
     break;
     case 'line4Adj':
-    line4.splice(index4, 1, loveAdjectives[words][ramWord])
+    theLine.splice(theIndex[3], 1, loveAdjectives[words][ramWord])
     break;
     default:
     console.log('not enough information')          
@@ -79,9 +83,7 @@ for (words in loveAdjectives) {
     }
 }
 
-const all = [line1, line2, line3, line4];
-
-const finalMessage = all.flat().join(' ');
+const finalMessage = theLine.join(' ');
 
 function finalLoveMessage () {
     console.log(finalMessage);
